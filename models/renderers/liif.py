7d8759cbd9c4605c178c37512871509baa1490f1
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-11-18 16:51:05 +0900

    Add liif renderer

--- models/renderers/liif.py
+++ models/renderers/liif.py
@@ -1,78 +1,141 @@
+import copy
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-import numpy as np
-
-import utils
 import models
 from models import register
+from utils.geometry_liif import convert_liif_feat_coord_cell, convert_posenc
 
 
 @register('liif')
-class liif(nn.Module):
+class LIIF(nn.Module):
 
-    def __init__(self, conv, imnet_spec=None):
-        self.conv_in = nn.Conv2d(conv.in_channels, conv.out_channels, 3)
-        self.imnet = models.make(imnet_spec)
+    def __init__(self, net, z_dec_channels, co_pe_dim=None, co_pe_w_max=None, ce_pe_dim=None, ce_pe_w_max=None, x_channels=None):
+        super().__init__()
+        self.x_channels = x_channels
+        coord_dim = 0
 
-    def forward(self, feat, coord, cell=None):
+        self.co_pe_dim = co_pe_dim
+        self.co_pe_w_max = co_pe_w_max
+        coord_dim += 2 if co_pe_dim is None else 2 * co_pe_dim
 
-        feat = self.conv_in(feat)
+        self.ce_pe_dim = ce_pe_dim
+        self.ce_pe_w_max = ce_pe_w_max
+        coord_dim += 2 if ce_pe_dim is None else 2 * ce_pe_dim
 
-        vx_lst = [-1, 1]
-        vy_lst = [-1, 1]
-        rx = 2 / feat.shape[-2] / 2
-        ry = 2 / feat.shape[-1] / 2
-        eps_shift = 1e-6
+        net_spec = copy.copy(net)
+        net_spec['args']['in_channels'] = (x_channels if x_channels is not None else 0) + z_dec_channels + coord_dim
+        self.net = models.make(net_spec)
 
-        b, c, h, w = feat.shape
+    def get_last_layer_weight(self):
+        return self.net.get_last_layer_weight()
 
-        feat_coord = utils.make_coord((h, w), flatten=False) \
-            .cuda().permute(2, 0, 1).unsqueeze(0).expand(b, 2, h, w)
-        
-        preds = []
-        areas = []
-        for vx in vx_lst:
-            for vy in vy_lst:
-                coord_ = coord.clone()
-                coord_[:, :, 0] += vx * rx + eps_shift
-                coord_[:, :, 1] += vy * ry + eps_shift
-                coord_.clamp_(-1 + 1e-6, 1 - 1e-6)
+    def forward(self, z_dec, coord, cell):
+        q_feat, rel_coord, rel_cell = convert_liif_feat_coord_cell(z_dec, coord, cell)
+        if self.co_pe_dim is not None:
+            rel_coord = convert_posenc(rel_coord, self.co_pe_dim, self.co_pe_w_max)
+        if self.ce_pe_dim is not None:
+            rel_cell = convert_posenc(rel_cell, self.ce_pe_dim, self.ce_pe_w_max)
+        layout = torch.cat([q_feat, rel_coord, rel_cell], dim=-1).permute(0, 3, 1, 2)
+        return self.net(layout)
 
-                q_feat = F.grid_sample(
-                    feat, coord_.flip(-1).unsqueeze(1),
-                    mode='nearest', align_corners=False
-                )[:, :, 0, :].permute(0, 2, 1)
-                
-                q_coord = F.grid_sample(
-                    feat_coord, coord_.flip(-1).unsqueeze(1),
-                    mode='nearest', align_corners=False
-                )[:, :, 0, :].permute(0, 2, 1)
 
-                rel_coord = coord - q_coord
-                rel_coord[:, :, 0] *= feat.shape[-2]
-                rel_coord[:, :, 1] *= feat.shape[-1]
-                inp = torch.cat([q_feat, rel_coord], dim=-1)
+@register('simple_renderer_net')
+class SimpleRendererNet(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels=3, kernel_size=3, normalize=True, *args, **kwargs):
+        super().__init__()
+        self.model = nn.ModuleList([nn.Conv2d(in_channels, hidden_channels, kernel_size, padding=(kernel_size - 1) // 2),
+                                    ResnetBlock(in_channels=hidden_channels,
+                                                out_channels=hidden_channels,
+                                                kernel_size=kernel_size,
+                                                temb_channels=0, dropout=0.0, normalize=normalize),
+                                    ResnetBlock(in_channels=hidden_channels,
+                                                out_channels=hidden_channels,
+                                                kernel_size=kernel_size,
+                                                temb_channels=0, dropout=0.0, normalize=normalize)])
+        self.norm_out = Normalize(hidden_channels) if normalize else torch.nn.Identity()
+        self.conv_out = torch.nn.Conv2d(hidden_channels, out_channels, kernel_size, padding=(kernel_size - 1) // 2)
 
-                if cell is not None:
-                    rel_cell = cell.clone()
-                    rel_cell[:, :, 0] *= feat.shape[-2]
-                    rel_cell[:, :, 1] *= feat.shape[-1]
-                    inp = torch.cat([inp, rel_cell], dim=-1)
-                
-                bs, q = coord.shape[:2]
-                pred = self.imnet(inp.view(bs * q, -1)).view(bs, q, -1)
-                preds.append(pred)
+    def get_last_layer_weight(self):
+        return self.conv_out.weight
 
-                area = torch.abs(rel_coord[:, :, 0] * rel_coord[:, :, 1])
-                areas.append(area + 1e-9)
-        
-        tot_area = torch.stack(areas).sum(dim=0)
-        t = areas[0]; areas[0] = areas[3]; areas[3] = t
-        t = areas[1]; areas[1] = areas[2]; areas[2] = t
-        
-        ret = 0
-        for pred, area in zip(preds, areas):
-            ret = ret + pred * (area / tot_area).unsqueeze(-1)
-        return ret
+    def forward(self, x):
+        for i, layer in enumerate(self.model):
+            if i in [1, 2]:
+                x = layer(x, None)
+            else:
+                x = layer(x)
+
+        h = self.norm_out(x)
+        h = nonlinearity(h)
+        x = self.conv_out(h)
+        return x
+    
+def Normalize(in_channels, num_groups=32):
+    return torch.nn.GroupNorm(num_groups=num_groups, num_channels=in_channels, eps=1e-6, affine=True)
+
+def nonlinearity(x):
+    # swish
+    return x*torch.sigmoid(x)
+
+class ResnetBlock(nn.Module):
+    def __init__(self, *, in_channels, out_channels=None, kernel_size=3, conv_shortcut=False,
+                 dropout, temb_channels=512, normalize=True):
+        super().__init__()
+        self.in_channels = in_channels
+        out_channels = in_channels if out_channels is None else out_channels
+        self.out_channels = out_channels
+        self.use_conv_shortcut = conv_shortcut
+
+        self.norm1 = Normalize(in_channels) if normalize else torch.nn.Identity()
+        self.conv1 = torch.nn.Conv2d(in_channels,
+                                     out_channels,
+                                     kernel_size=kernel_size,
+                                     stride=1,
+                                     padding=(kernel_size - 1) // 2)
+        if temb_channels > 0:
+            self.temb_proj = torch.nn.Linear(temb_channels,
+                                             out_channels)
+        self.norm2 = Normalize(out_channels) if normalize else torch.nn.Identity()
+        self.dropout = torch.nn.Dropout(dropout)
+        self.conv2 = torch.nn.Conv2d(out_channels,
+                                     out_channels,
+                                     kernel_size=kernel_size,
+                                     stride=1,
+                                     padding=(kernel_size - 1) // 2)
+        if self.in_channels != self.out_channels:
+            if self.use_conv_shortcut:
+                self.conv_shortcut = torch.nn.Conv2d(in_channels,
+                                                     out_channels,
+                                                     kernel_size=kernel_size,
+                                                     stride=1,
+                                                     padding=(kernel_size - 1) // 2)
+            else:
+                self.nin_shortcut = torch.nn.Conv2d(in_channels,
+                                                    out_channels,
+                                                    kernel_size=1,
+                                                    stride=1,
+                                                    padding=0)
+
+    def forward(self, x, temb):
+        h = x
+        h = self.norm1(h)
+        h = nonlinearity(h)
+        h = self.conv1(h)
+
+        if temb is not None:
+            h = h + self.temb_proj(nonlinearity(temb))[:,:,None,None]
+
+        h = self.norm2(h)
+        h = nonlinearity(h)
+        h = self.dropout(h)
+        h = self.conv2(h)
+
+        if self.in_channels != self.out_channels:
+            if self.use_conv_shortcut:
+                x = self.conv_shortcut(x)
+            else:
+                x = self.nin_shortcut(x)
+
+        return x+h
