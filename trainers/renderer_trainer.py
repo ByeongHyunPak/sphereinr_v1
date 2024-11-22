72d2b62d8b4d45fc56a662fa3d8215be623bb070
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-11-22 11:54:09 +0900

    visualize_ae

--- trainers/renderer_trainer.py
+++ trainers/renderer_trainer.py
@@ -15,36 +15,8 @@ from trainers.base_trainer import BaseTrainer
 @register('renderer_trainer')
 class RendererTrainer(BaseTrainer):
 
-    def prepare_visualize(self):
-        self.vis_spec = dict()
-
-        def get_samples(dataset, s):
-            n = len(dataset)
-            lst = [dataset[i] for i in list(range(0, n, n // s))[:s]]
-            data = dict()
-            for k in lst[0].keys():
-                data[k] = torch.stack([_[k] for _ in lst]).cuda()
-            return data
-
-        self.vis_spec['ds_samples'] = self.cfg.visualize.get('ds_samples', 0)
-        if self.vis_spec['ds_samples'] > 0:
-            self.vis_ds_samples = {'train': get_samples(self.datasets['train'], self.vis_spec['ds_samples'])}
-            if self.datasets.get('val') is not None:
-                self.vis_ds_samples['val'] = get_samples(self.datasets['val'], self.vis_spec['ds_samples'])
-
     def make_datasets(self):
         super().make_datasets()
-
-        self.vis_resolution = self.cfg.visualize.resolution
-        if isinstance(self.vis_resolution, int):
-            self.vis_resolution = (self.vis_resolution, self.vis_resolution)
-        if self.is_master:
-            random.seed(0) # to get a fixed vis set from wrapper_cae
-            # self.prepare_visualize()
-            if self.cfg.random_seed is not None:
-                random.seed(self.cfg.random_seed + self.rank)
-            else:
-                random.seed()
 
     def make_model(self, model_spec=None):
         super().make_model(model_spec)
@@ -97,48 +69,11 @@ class RendererTrainer(BaseTrainer):
 
     def visualize(self):
         self.model_ddp.eval()
+
         if self.is_master:
             with torch.no_grad():
                 if self.vis_spec['ds_samples'] > 0:
                     self.visualize_ae()
-        if self.distributed:
-            dist.barrier()
-
-    def visualize_ae_(self, name, data, bs=1):
-        gt = data['gt']
-        n = data['inp'].shape[0]
-        pred = []
-        center_zoom = []
-
-        for i in range(0, n, bs):
-            d = {k: v[i: min(i + bs, n)] for k, v in data.items()}
-            pred.append(self.model(d, mode='pred'))
-
-            if (self.vis_ae_center_zoom_res is not None) and (not name.endswith('_whole')):
-                r0 = self.vis_resolution[0] / self.vis_ae_center_zoom_res
-                r1 = self.vis_resolution[1] / self.vis_ae_center_zoom_res
-                d['gt_coord'], d['gt_cell'] = make_coord_cell_grid(
-                    self.vis_resolution, [[-r0, r0], [-r1, r1]], device=d['gt_coord'].device, bs=d['gt_coord'].shape[0])
-                center_zoom.append(self.model(d, mode='pred'))
-
-        pred = torch.cat(pred, dim=0)
-        if self.is_master:
-            vimg = []
-            for i in range(len(gt)):
-                vimg.extend([pred[i], gt[i]])
-            vimg = torch.stack(vimg)
-            vimg = torchvision.utils.make_grid(vimg, nrow=4, normalize=True, value_range=(-1, 1))
-            self.log_image(name, vimg)
-
-        if (self.vis_ae_center_zoom_res is not None) and (not name.endswith('_whole')):
-            center_zoom = torch.cat(center_zoom, dim=0)
-            if self.is_master:
-                vimg = []
-                for i in range(len(gt)):
-                    vimg.extend([center_zoom[i], center_zoom[i]])
-                vimg = torch.stack(vimg)
-                vimg = torchvision.utils.make_grid(vimg, nrow=4, normalize=True, value_range=(-1, 1))
-                self.log_image(name + '_center_zoom', vimg)
 
     def visualize_ae(self):
         for split in ['train', 'val']:
@@ -147,9 +82,20 @@ class RendererTrainer(BaseTrainer):
             data = self.vis_ds_samples[split]
             self.visualize_ae_(split, data)
 
-            if self.cfg.visualize.get('vis_ae_whole', False):
-                x = data['inp']
-                coord, cell = make_coord_cell_grid(x.shape[-2:], device=x.device, bs=x.shape[0])
-                data_whole = {'inp': x, 'gt': x, 'gt_coord': coord, 'gt_cell': cell}
-                self.visualize_ae_(split + '_whole', data_whole)
+    def visualize_ae_(self, name, data, bs=1):
+        gt = data['gt']
+        n = data['inp'].shape[0]
+        pred = []
 
+        for i in range(0, n, bs):
+            d = {k: v[i: min(i + bs, n)] for k, v in data.items()}
+            pred.append(self.model(d, mode='pred'))
+            
+        pred = torch.cat(pred, dim=0)
+        if self.is_master:
+            vimg = []
+            for i in range(len(gt)):
+                vimg.extend([pred[i], gt[i]])
+            vimg = torch.stack(vimg)
+            vimg = torchvision.utils.make_grid(vimg, nrow=4, normalize=True, value_range=(-1, 1))
+            self.log_image(name, vimg)
